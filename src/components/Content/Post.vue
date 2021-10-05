eed0122d8d49a06a50f4a6c4f6ea200a59497b6d
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-05 23:30:58 +0300

    Set content components

---
+++ src/components/Content/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-md-auto pt-3">
+    <div class="card">
+      <div class="card-body" style="width:20rem">
+        <h5 class="card-title">
+          {{ post.title }}
+        </h5>
+        <p class="card-text">
+          {{ post.body }}
+        </p>
+        <a :href="'/posts/' + post.id" class="card-link">View</a>
+        <a :href="'/posts/' + post.id" class="card-link text-danger">Delete</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+};
+</script>
---
+++ src/components/Content/Posts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row justify-content-md-center">
+    <Post v-for="post in posts" :post="post" :key="post.id" />
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Content/Post";
+import axios from "axios";
+export default {
+  components: { Post },
+  data() {
+    return { posts: [] };
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        for (let key in response.data) {
+          this.posts.push(response.data[key]);
+        }
+      })
+      .catch((e) => console.log(e));
+  },
+};
+</script>
