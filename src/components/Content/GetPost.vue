f6c5eb294c92c863fcb06e23a9cab1a49f75967b
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-06 09:48:12 +0300

    Refolder components

---
+++ src/components/Content/GetPost.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-md-auto pt-3">
+    <div class="card">
+      <div class="card-body" style="width:20rem">
+        <h5 class="card-title">
+          {{ post.title }}
+        </h5>
+        <p class="card-text">
+          {{ post.body }}
+        </p>
+        <a :href="'/posts/' + post.id" class="card-link">View</a>
+        <a :href="'/posts/' + post.id" class="card-link text-danger">Delete</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Home",
+  components: {},
+  created() {
+    axios
+      .get(
+        "https://jsonplaceholder.typicode.com/posts/" + this.$route.params.id
+      )
+      .then((response) => {
+        this.post = response.data;
+      })
+      .catch((e) => console.log(e));
+  },
+  data() {
+    return {
+      post: {},
+    };
+  },
+};
+</script>
+/* background-image: linear-gradient( to right top, #051937, #002436, #022b2d,
+#293928, #32342d );*/
